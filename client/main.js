9476504afe2d32e6c097d06cff596ceaeecf0998
youngmoon <young.utf@gmail.com>
2014-12-06 14:58:37 +0900

    responsive

--- client/main.js
+++ client/main.js
@@ -1,6 +1,20 @@
 /**
  * Created by youngmoon on 12/3/14.
  */
+
+Meteor.startup(function() {
+
+})
+
+Template.main.rendered = function() {
+
+};
+//(function() {
+//
+//  Session.set('loginUser', Users.findOne());
+//  console.log('haha');
+//  console.log(Session.get('loginUser'));
+//}());
 Template.main.helpers({
 
 })
--- client/views/posts/write_post.js
+++ client/views/posts/write_post.js
@@ -6,3 +6,18 @@ Template.writePost.helpers({
     return Users.findOne();
   }
 })
+
+Template.writePost.events = {
+  'click .post-button': function(evt, tmpl) {
+    //console.log(evt);
+    //console.log(tmpl);
+    Session.set('loginUser', Users.find({userName:'youngmoon'}).fetch());
+    Deps.autorun( function() {console.log(Session.get('loginUser'));} );
+    alert($(window).width());
+  }
+}
+
+Template.writePost.rendered = function() {
+  Session.set('loginUser', Users.find({userName:'youngmoon'}).fetch());
+  console.log(Session.get('loginUser'));
+}
